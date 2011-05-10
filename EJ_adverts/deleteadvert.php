32653dc10a7eee5585ad78463213531b36ed57f0
Elliott Bristow <admin@jigsawspain.com>
2011-05-10 12:38:08 +0200

    Deleting advert now deletes associates images

--- EJ_adverts/deleteadvert.php
+++ EJ_adverts/deleteadvert.php
@@ -9,5 +9,14 @@ if ($_SESSION['key'] != $_POST['key'] or $_POST['key']=="")
 	require('../../init.inc.php');
 	$query = "DELETE FROM {$EJ_mysql->prefix}module_EJ_adverts WHERE EJ_advertId = ".$_POST['id'];
 	$EJ_mysql->query($query);
+	if (is_dir('images/'.$_POST['id']))
+	{
+		$dir = opendir('images/'.$_POST['id']);
+		while ($file = readdir($dir))
+		{
+			unlink('images/'.$_POST['id'].'/'.$file);
+		}
+		rmdir('images/'.$_POST['id']);
+	}
 }
 ?>
